90a9f6997b4497fc4f12b8d14f8c2a3c02ef0d95
gulci <gulci@LAPTOP-KPNR185M>
2022-12-02 17:54:13 +0300

    Proje dosyası ekle.

---
+++ VizeCalismasi/Program.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VizeCalismasi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //bool a=new bool();
+            //byte a1 = new byte();
+            //char a2 = new char();
+            //decimal a3 = new decimal();
+            //double a4 = new double();
+            //float a5 = new float();
+            //int a6 = new int(); 
+
+
+
+            //Console.WriteLine(a);
+            //Console.WriteLine(a1);
+            //Console.WriteLine(a2);
+            //Console.WriteLine(a3);
+            //Console.WriteLine(a4);
+            //Console.WriteLine(a5);
+            //Console.WriteLine(a6);
+            //Console.ReadLine();
+
+            //int x = 5;
+            //float a=2;
+            //a = x;
+
+
+
+
+            //Console.WriteLine(a);
+
+
+
+
+            //float f = 20f;
+            //double d;
+            //d = f;
+            //Console.WriteLine(d);
+
+            //char c = 'A';
+            //decimal m;
+            //m = c;
+            //Console.WriteLine(m);
+
+
+
+
+            //int i1 = 255;
+            //int i2 = 500;
+            //byte b, c;
+
+
+
+            //checked
+            //{
+            //    b = (byte)i1;
+            //    Console.WriteLine(b);
+
+
+            //    unchecked
+            //    {
+            //        c = (byte)i2;
+            //    }
+
+            //    Console.WriteLine(c);
+
+
+            //}
+
+
+            //int a = 5;
+            //int b = 7;
+            //string a1 = a.ToString();
+            //string b1 = b.ToString();
+            //Console.WriteLine(a + b);
+            //Console.WriteLine(a1 + b1);
+
+            //int i = 10;
+            //object o = i;
+
+            //long l = (long)o;
+
+
+            //Console.WriteLine(i);
+            //Console.WriteLine(l);
+
+            //string s1, s2;
+            //int i1, i2, t;
+
+            //Console.WriteLine("1.sayıyı giriniz:");
+            //s1 = Console.ReadLine();
+
+            //Console.WriteLine("2.sayıyı giriniz:");
+            //s2 = Console.ReadLine();
+
+            //i1 = Convert.ToInt32(s1);
+            //i2 = Convert.ToInt32(s2);
+
+
+            //t = i1 + i2;
+
+            //Console.WriteLine("Toplam=" + t.ToString());
+
+
+            //bool a = true;
+            //bool b = false;
+            //if (a ^ b) Console.WriteLine("a^ b doğrudur");
+
+            //byte b1 = 254;
+            //byte b2 = (byte)~b1;
+            //Console.WriteLine(b2);
+
+            //byte a = 153;
+            //byte b = 104;
+            //byte x = (byte)(a & b);
+            //Console.WriteLine(x);   
+
+            //string s;
+            //for(s=Console.ReadLine(); s!="çıkış"; s=Console.ReadLine())
+            //    Console.WriteLine(s);
+
+            //Random rnd = new Random();
+            //int[] dizi = new int[3];
+            //dizi[0] = rnd.Next(2, 10);
+            //dizi[1] = rnd.Next(50);
+            //dizi[2] = rnd.Next();
+
+            //foreach(int i in dizi)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+
+            //int[,] dizi = new int[4, 4];
+            //int k = 1;
+            //for (int i = 0; i < 4; i++)
+
+
+
+            //    for (int j = i; j < 4; j++)
+            //    {
+            //        dizi[i, j] = k;
+            //        dizi[j, i] = k;
+            //        k++;
+
+            //    }
+
+            //    k = 1;
+            //    foreach (int x in dizi)
+            //    {
+
+            //        if (k % 4 == 0)
+            //        {
+            //            Console.Write(x + "");
+            //            Console.WriteLine();
+            //        }
+            //        else
+            //            Console.Write(x + "");
+            //        k++;
+
+            //    }
+
+
+
+
+            //int[][] dizi = new int[3][];
+            //dizi[0] = new int[5];
+            //dizi[1] = new int[3];
+            //dizi[2] = new int[4];
+            //Console.WriteLine(dizi.GetLength(0)); // dizinin satır sayısı
+            //Console.WriteLine(dizi[2].GetLength(0));
+
+
+            //int[] dizi1 = { 1, 2, 3, 4, 5, 6, 7 };
+            //int dizi2 = new int [10];
+            //dizi1.CopyTo(dizi2, 3);
+
+
+            //string[] dizi = { "za","zb", "e", "b", "a", "c" };
+
+            //Console.WriteLine("dizinin elemanları");
+            //foreach (string s in dizi)
+            //    Console.WriteLine(s);
+
+            //Array.Sort(dizi);
+            //Console.WriteLine();
+
+            //Console.WriteLine("Sıralanmış dizi");
+
+            //foreach (string s in dizi)
+            //    Console.WriteLine(s);
+
+
+
+            //ArrayList liste = new ArrayList();
+            //liste.Add("Murat");
+            //liste.Add("Yeşim");
+            //liste.Add("Deniz");
+            //Console.WriteLine(liste[2]);
+            //foreach (String eleman in liste)
+            //{ Console.WriteLine(eleman); }
+
+           
+
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
